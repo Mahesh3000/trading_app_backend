f5d3cee12028f3a9d0852572f612713c49f672c9
Mahesh3000 <mahesh.sivngi@gmail.com>
2025-04-17 18:39:33 -0500

    amplify changes

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 8000;
 // Middlewares
 const helmet = require("helmet");
 const corsOptions = {
-  origin: "https://maheshsivangi.tech:5173", // Replace with your actual frontend URL
+  origin: "https://maheshsivangi.tech", // Replace with your actual frontend URL
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true, // Allow cookies/auth headers
 };
